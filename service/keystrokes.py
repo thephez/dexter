a21dc96f25377845c427359a3b4f424ed1910642
thephez <thephez@users.noreply.github.com>
2023-12-28 13:24:49 -0500

    refactor: improve perf with dict

--- service/keystrokes.py
+++ service/keystrokes.py
@@ -6,33 +6,32 @@ from dexter.core.util import get_pygame, fuzzy_list_range, to_alphanumeric
 
 from playsound import playsound
 
-_KEY_ACTION_PHRASES = (
-    # Add phrases and corresponding keyboard actions here
-    ("Copy that", "ctrl+c", False),
-    ("Paste that", "ctrl+v", False),
-    ("Copy from terminal", "ctrl+shift+c", False),
-    ("Paste to terminal", "ctrl+shift+v", False),
-    ("Refresh", "f5", False),
-    ("Open terminal", "ctrl+alt+t", False),
-    ("Show applications", "win", False),
-    ("Show notifications", "win+m", False),
-    ("Open System Monitor", "win+2", False),
-    ("Open Firefox", "win+3", False),
-    ("Open Chrome", "win+6", False),
-    ("Open Thunderbird", "win+7", False),
-    ("Open Code", "win+8", False),
-    ("Open Signal", "win+9", False),
-    ("Open Slack", "ctrl+alt+shift+s", False),
-    ("Next application", "alt+tab", False),
-    ("Last application", "alt+shift+tab", False),
-    ("Next window", "alt+`", False),
-    ("Last window", "alt+shift+`", False),
-    ("Press escape key", "esc", False),
-    ("Move up", "up", False),
-    ("Move down", "down", False),
-    ("Page up", "pageup", False),
-    ("Page down", "pagedown", False),
-)
+_KEY_ACTION_PHRASES_DICT = {
+    "Copy that": ("ctrl+c", False),
+    "Paste that": ("ctrl+v", False),
+    "Copy from terminal": ("ctrl+shift+c", False),
+    "Paste to terminal": ("ctrl+shift+v", False),
+    "Refresh": ("f5", False),
+    "Open terminal": ("ctrl+alt+t", False),
+    "Show applications": ("win", False),
+    "Show notifications": ("win+m", False),
+    "Open System Monitor": ("win+2", False),
+    "Open Firefox": ("win+3", False),
+    "Open Chrome": ("win+6", False),
+    "Open Thunderbird": ("win+7", False),
+    "Open Code": ("win+8", False),
+    "Open Signal": ("win+9", False),
+    "Open Slack": ("ctrl+alt+shift+s", False),
+    "Next application": ("alt+tab", False),
+    "Last application": ("alt+shift+tab", False),
+    "Next window": ("alt+`", False),
+    "Last window": ("alt+shift+`", False),
+    "Press escape key": ("esc", False),
+    "Move up": ("up", False),
+    "Move down": ("down", False),
+    "Page up": ("pageup", False),
+    "Page down": ("pagedown", False),
+}
 
 class _KeyboardActionHandler(Handler):
     def __init__(self, service, tokens, reply, belief):
@@ -46,26 +45,24 @@ class KeyboardActionService(Service):
     def __init__(self, state, belief=0.80):
         super().__init__("KeyboardAction", state)
         self._belief = float(belief)
-        self._key_action_phrases = [
-            (self._split_phrase(phrase), key_action, is_prefix)
-            for phrase, key_action, is_prefix in _KEY_ACTION_PHRASES
-        ]
+        self._key_action_phrases_dict = {
+            self._split_phrase(phrase): (key_action, is_prefix)
+            for phrase, (key_action, is_prefix) in _KEY_ACTION_PHRASES_DICT.items()
+        }
 
     def _split_phrase(self, phrase):
         return tuple(to_alphanumeric(word) for word in phrase.split())
 
     def evaluate(self, tokens):
         words = self._words(tokens)
-        for phrase, key_action, is_prefix in self._key_action_phrases:
+        for phrase, (key_action, is_prefix) in self._key_action_phrases_dict.items():
             try:
                 start, end, score = fuzzy_list_range(words, phrase)
                 if start == 0 and (not is_prefix or end == len(phrase)):
                     self._execute_key_action(key_action)
-                    # return _KeyboardActionHandler(self, tokens, f"Executed {key_action}", self._belief)
-                    # Don't speak a reply for successful actions, but play a sound instead
-                    playsound('sounds/keystroke-success.ogg')
+                    # playsound('sounds/keystroke-success.ogg')
                     return _KeyboardActionHandler(self, tokens, None, self._belief)
-            except ValueError as e:
+            except ValueError:
                 continue
         # playsound('sounds/keystroke-failure.ogg')
         return None
